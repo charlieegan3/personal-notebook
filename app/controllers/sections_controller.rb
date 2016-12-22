d9caad70837714435169c499086bd61a9d30b78b
Charlie Egan <charlieegan3@users.noreply.github.com>
2016-12-22 12:12:26 +0000

    Dry up & improve section view

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -4,7 +4,7 @@ class SectionsController < ApplicationController
   # GET /sections
   # GET /sections.json
   def index
-    @sections = Section.where(parent_id: nil, user: current_user)
+    @sections = Section.includes(:children).where(parent_id: nil, user: current_user)
   end
 
   # GET /sections/1
@@ -66,7 +66,7 @@ class SectionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_section
-      @section = Section.find(params[:id])
+      @section = Section.includes(:children, :notes).find(params[:id])
       redirect_to sections_path, notice: 'Nope.' unless @section.user == current_user
     end
 
