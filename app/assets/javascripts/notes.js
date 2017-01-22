8783011d9e160054bc51b9cd6145569837b860a2
Charlie Egan <charlieegan3@users.noreply.github.com>
2017-01-22 14:25:23 +0000

    Remove password features

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,21 +1,7 @@
-function append_password() {
-  $.getJSON('/words.json', function(words) {
-    var phrase = [];
-    for (var i = 0; i < 4; i++) {
-      phrase.push(words[Math.floor(Math.random()*words.length)]);
-    }
-
-    $("#note_content").val($("#note_content").val() + "\n\n" + phrase.join(' '));
-  });
-}
-
 $(document).on('ready page:load turbolinks:load', function() {
   $("#expand_textarea").click(function() {
     var textarea = $("#note_content");
-    var new_row_count = parseInt(textarea.attr("rows")) + 1;
+    var new_row_count = parseInt(textarea.attr("rows")) + 2;
     textarea.attr("rows", new_row_count.toString());
   });
-
-  $("#generate_password").unbind('click', append_password);
-  $("#generate_password").bind('click', append_password);
 });
