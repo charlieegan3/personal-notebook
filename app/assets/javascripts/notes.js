b9dbc7fc145870c2b5681f6bf6f5577c2f28d147
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-02-03 21:47:28 +0000

    Make text area's larger

--- app/assets/javascripts/notes.js
+++ app/assets/javascripts/notes.js
@@ -1,9 +1,9 @@
 $(document).on('ready page:load turbolinks:load', function() {
   $.each($('textarea'), function() {
-	var offset = this.offsetHeight - this.clientHeight;
-	var resizeTextarea = function(el) {
-	  $(el).css('height', 'auto').css('height', el.scrollHeight + offset);
-	};
-	$(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
+    var offset = this.offsetHeight - this.clientHeight;
+    var resizeTextarea = function(el) {
+      $(el).css('height', 'auto').css('height', el.scrollHeight + offset + 100);
+    };
+    $(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
   });
 });
