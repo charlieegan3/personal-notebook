ed9bb5e8a2d5251488772866bcc9eefbf2e5aa0e
Charlie Egan <charlieegan3@users.noreply.github.com>
2018-02-03 23:23:28 +0000

    Remove auto resize note input

--- app/assets/javascripts/notes.js
+++
@@ -1,9 +0,0 @@
-$(document).on('ready page:load turbolinks:load', function() {
-  $.each($('textarea'), function() {
-    var offset = this.offsetHeight - this.clientHeight;
-    var resizeTextarea = function(el) {
-      $(el).css('height', 'auto').css('height', el.scrollHeight + offset + 100);
-    };
-    $(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
-  });
-});
